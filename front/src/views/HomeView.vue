da1d251ace40336d7e6e1da986c09f2fd8f8dd40
EJ <hanej93@naver.com>
2023-05-13 16:31:49 +0900

    글 리스트 화면 만들기

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,7 +1,36 @@
 <script setup lang="ts">
+import axios from "axios";
+import {ref} from "vue";
+
+const posts = ref([]);
+
+axios.get('/api/posts')
+.then((res) => {
+    res.data.forEach((r: any) => {
+        posts.value.push(r);
+    });
+});
 </script>
 
 <template>
-  <main>
-  </main>
+    <ul>
+        <li v-for="post in posts" :key="post.id">
+            <div>
+                {{post.title}}
+            </div>
+            <div>
+                {{post.content}}
+            </div>
+        </li>
+    </ul>
 </template>
+
+<style scoped>
+li {
+    margin-bottom: 1rem;
+}
+
+li:last-child {
+    margin-bottom: 0;
+}
+</style>
